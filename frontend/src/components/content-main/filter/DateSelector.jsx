bbee162250b1245142c60f188930e94d8307fcb2
defcarlito <harrisoncamorim@gmail.com>
2025-06-26 15:38:31 -0500

    styled date selector

--- frontend/src/components/content-main/filter/DateSelector.jsx
+++ frontend/src/components/content-main/filter/DateSelector.jsx
@@ -2,13 +2,16 @@ import { Box, Button, HStack } from "@chakra-ui/react";
 
 function DateSelector({ dates, selectedDate, setSelectedDate, formatDate }) {
 
+
     return (
         <HStack p={4}>
             {dates.map((date, index) => (
-                <Button size={"sm"} color={"white"} variant={"plain"} 
-                onClick={() => setSelectedDate(date)} key={index}>
-                    {formatDate(date)}
-                </Button>
+                    <Button size={"sm"} variant={"plain"} key={index} 
+                    onClick={() => setSelectedDate(date)}
+                    color={selectedDate === date ? "white" : "gray.700"}
+                    _hover={{ color: selectedDate !== date && "gray.500" }}>
+                        {formatDate(date)}
+                    </Button>
             ))}
             <Button size={"sm"} color={"blue.200"} variant={"plain"}>View more</Button>
             
