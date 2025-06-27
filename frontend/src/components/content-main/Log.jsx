4475f40d40e82cefea6d08e424ebe0260eb81817
defcarlito <harrisoncamorim@gmail.com>
2025-06-27 11:48:30 -0500

    moved query logic to dashboard to expand scope of query data

--- frontend/src/components/content-main/Log.jsx
+++ frontend/src/components/content-main/Log.jsx
@@ -1,19 +1,9 @@
 import { Flex, Heading, HStack, VStack, Text, Box, Presence } from "@chakra-ui/react"
 import Match from "./match/Match"
-import { useEffect, useState } from "react";
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
-import { db } from "../Firebase";
 import DateSelector from "./filter/DateSelector";
 import FilterPlaylistMenu from "./filter/FilterPlaylistMenu";
 
-function Log() {
-
-    const [dates, setDates] = useState([])
-
-    const [selectedDate, setSelectedDate] = useState("")
-    const [selectedPlaylist, setSelectedPlaylist] = useState(10) // default = ranked 1v1
-
-    const [allMatchesOnDate, setAllMatchesOnDate] = useState([])
+function Log({ dates, selectedDate, setSelectedDate, setSelectedPlaylist, allMatchesOnDate }) {
 
     function formatDate(date) {
         const parts = date.split("-")
@@ -22,33 +12,6 @@ function Log() {
         const day = parseInt(parts[2])
         return `${month}/${day}/${year}`
     }
-    
-    useEffect(() => {
-        const fetchAvaliableDates = async () => {
-            const snapshot = await getDocs(collection(db, "match_dates"))
-            const fetchedDates = snapshot.docs.map(doc => doc.id)
-            const datesInOrder = fetchedDates.reverse()
-            setDates(datesInOrder)
-            fetchedDates.length > 0 && setSelectedDate(datesInOrder[0]) // set default
-        }
-        fetchAvaliableDates()
-    }, [])
-
-    useEffect(() => {
-        const fetchMatches = async () => {
-            const matchesRef = collection(db, "matches")
-            const q = query(
-                matchesRef, 
-                where("StartDate", "==", selectedDate),
-                where("Playlist", "==", selectedPlaylist),
-                orderBy("StartEpoch", "desc")
-            )
-            const snapshot = await getDocs(q)
-            const matches = snapshot.docs.map(doc => doc.data())
-            setAllMatchesOnDate(matches)
-        }
-        fetchMatches()
-    }, [selectedDate, selectedPlaylist])
 
     return (
         <VStack w={"100%"} minH={"100vh"} px={8} py={4} gap={2}>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,12 +2,48 @@ import { Box, Grid, GridItem } from "@chakra-ui/react";
 import Navbar from "../components/nav/Navbar";
 import CurrentStats from "../components/content-left/CurrentStats";
 import Log from "../components/content-main/Log";
+import { db } from "../components/Firebase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 function Dashboard() {
+
     const centered = {
         display: "flex",
         justifyContent: "center"
     }
+    
+    const [dates, setDates] = useState([])
+    const [selectedDate, setSelectedDate] = useState("")
+    const [selectedPlaylist, setSelectedPlaylist] = useState(10) // default = ranked 1v1
+    const [allMatchesOnDate, setAllMatchesOnDate] = useState([])
+
+    useEffect(() => {
+        const fetchAvaliableDates = async () => {
+            const snapshot = await getDocs(collection(db, "match_dates"))
+            const fetchedDates = snapshot.docs.map(doc => doc.id)
+            const datesInOrder = fetchedDates.reverse()
+            setDates(datesInOrder)
+            fetchedDates.length > 0 && setSelectedDate(datesInOrder[0]) // set default
+        }
+        fetchAvaliableDates()
+    }, [])
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const matchesRef = collection(db, "matches")
+            const q = query(
+                matchesRef, 
+                where("StartDate", "==", selectedDate),
+                where("Playlist", "==", selectedPlaylist),
+                orderBy("StartEpoch", "desc")
+            )
+            const snapshot = await getDocs(q)
+            const matches = snapshot.docs.map(doc => doc.data())
+            setAllMatchesOnDate(matches)
+        }
+        fetchMatches()
+    }, [selectedDate, selectedPlaylist])
     
     return (
         <Box bg={"gray.900"} color={"white"} minH={"100vh"}>
@@ -17,7 +53,12 @@ function Dashboard() {
                     <CurrentStats />
                 </GridItem>
                 <GridItem borderX={"1px solid"} borderColor={"gray.700"}>
-                    <Log />
+                    <Log 
+                    dates={dates} 
+                    selectedDate={selectedDate}
+                    setSelectedDate={setSelectedDate}
+                    allMatchesOnDate={allMatchesOnDate} 
+                    setSelectedPlaylist={setSelectedPlaylist}/>
                 </GridItem>
                 <GridItem {...centered}>
 
